78b34b89229ea1df78b023e29102157b9dddd184
DrChhun <Chhunfindsome1@gmail.com>
2024-08-16 15:42:23 +0700

    fresh commit

---
+++ src/main/java/com/example/jpa_hw/JpaHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaHwApplication.class, args);
+    }
+
+}
